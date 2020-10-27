81310f3910ce119728774f37e8fd1e024ef9b2db
jiyeon <jyun3382@gmail.com>
2020-10-27 16:30:11 +0900

    Initial Commit

---
+++ LabProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LabProject
+//
+//  Created by 장지연 on 2020/10/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
